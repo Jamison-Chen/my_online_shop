897288da6f3be4167eeee072b00339061d13e58e
Jamison <106208004@g.nccu.edu.tw>
2022-03-03 11:55:50 +0800

    add dist

---
+++ precache-manifest.3223429e979f655a7119341160383e86.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35677db92e41b1ce522c",
+    "url": "/my_online_shop/css/app.d02b27fd.css"
+  },
+  {
+    "revision": "6a7b22e08452a4f6ff74",
+    "url": "/my_online_shop/css/chunk-034e5d78.79497e12.css"
+  },
+  {
+    "revision": "62f84c94515bbea2288c",
+    "url": "/my_online_shop/css/chunk-0b98e858.607dc157.css"
+  },
+  {
+    "revision": "c8b3cae2284fcb0c1b9d",
+    "url": "/my_online_shop/css/chunk-0ca18c55.54d6ea3c.css"
+  },
+  {
+    "revision": "354d9acf4384a7f4df34",
+    "url": "/my_online_shop/css/chunk-3abe67c2.02027578.css"
+  },
+  {
+    "revision": "de6c5b58f16529e7da67",
+    "url": "/my_online_shop/css/chunk-58d42ba4.466d9307.css"
+  },
+  {
+    "revision": "73cd1b8c1fc2ffcd86d2",
+    "url": "/my_online_shop/css/chunk-5d5cd5a5.78e5313f.css"
+  },
+  {
+    "revision": "2123bca584a727f58429",
+    "url": "/my_online_shop/css/chunk-5e62bab3.3b528931.css"
+  },
+  {
+    "revision": "9205c8da6a9addd47d94",
+    "url": "/my_online_shop/css/chunk-6c34d505.00299c86.css"
+  },
+  {
+    "revision": "0159cab462224b79aedf",
+    "url": "/my_online_shop/css/chunk-708c26e3.7b839f10.css"
+  },
+  {
+    "revision": "cccb6867cd2d9c316d93",
+    "url": "/my_online_shop/css/chunk-8b5790c0.97a674c1.css"
+  },
+  {
+    "revision": "12df25862538c44414b2",
+    "url": "/my_online_shop/css/chunk-b0be8f6a.15f7ca55.css"
+  },
+  {
+    "revision": "8dbe84bd9afd68ab231d2574ad212e69",
+    "url": "/my_online_shop/index.html"
+  },
+  {
+    "revision": "35677db92e41b1ce522c",
+    "url": "/my_online_shop/js/app.a73fddc2.js"
+  },
+  {
+    "revision": "6a7b22e08452a4f6ff74",
+    "url": "/my_online_shop/js/chunk-034e5d78.25699da2.js"
+  },
+  {
+    "revision": "62f84c94515bbea2288c",
+    "url": "/my_online_shop/js/chunk-0b98e858.5f1ae962.js"
+  },
+  {
+    "revision": "c8b3cae2284fcb0c1b9d",
+    "url": "/my_online_shop/js/chunk-0ca18c55.e4c12d9b.js"
+  },
+  {
+    "revision": "7aac84ad84b14fe84165",
+    "url": "/my_online_shop/js/chunk-2d0b9bb6.a7243537.js"
+  },
+  {
+    "revision": "9dfdafa7dad5198af8a4",
+    "url": "/my_online_shop/js/chunk-2d0db0b6.755f6a5f.js"
+  },
+  {
+    "revision": "1947a118ac979d0297ee",
+    "url": "/my_online_shop/js/chunk-2d222d07.95b7bc67.js"
+  },
+  {
+    "revision": "5c76d58d861c5128c52d",
+    "url": "/my_online_shop/js/chunk-2d22996d.c4118575.js"
+  },
+  {
+    "revision": "354d9acf4384a7f4df34",
+    "url": "/my_online_shop/js/chunk-3abe67c2.df8bb6a7.js"
+  },
+  {
+    "revision": "de6c5b58f16529e7da67",
+    "url": "/my_online_shop/js/chunk-58d42ba4.d1ea9d21.js"
+  },
+  {
+    "revision": "73cd1b8c1fc2ffcd86d2",
+    "url": "/my_online_shop/js/chunk-5d5cd5a5.9a3943a3.js"
+  },
+  {
+    "revision": "2123bca584a727f58429",
+    "url": "/my_online_shop/js/chunk-5e62bab3.02b31bc2.js"
+  },
+  {
+    "revision": "9205c8da6a9addd47d94",
+    "url": "/my_online_shop/js/chunk-6c34d505.2644c6de.js"
+  },
+  {
+    "revision": "0159cab462224b79aedf",
+    "url": "/my_online_shop/js/chunk-708c26e3.69dbf72a.js"
+  },
+  {
+    "revision": "cccb6867cd2d9c316d93",
+    "url": "/my_online_shop/js/chunk-8b5790c0.fb15555b.js"
+  },
+  {
+    "revision": "12df25862538c44414b2",
+    "url": "/my_online_shop/js/chunk-b0be8f6a.b1132060.js"
+  },
+  {
+    "revision": "4743ccbd6f507d35a754",
+    "url": "/my_online_shop/js/chunk-vendors.dae80dcb.js"
+  },
+  {
+    "revision": "925e541063bdd89c40a4a2b3f611450a",
+    "url": "/my_online_shop/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/my_online_shop/robots.txt"
+  }
+]);
